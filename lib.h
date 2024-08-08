e4f482933be111dd13f93db60656474349b1538d
Gabriel-Farias-UCB <gabriel.rfarias@a.ucb.br>
2024-08-08 14:12:41 -0300

    nao fiz o extrato ainda

--- lib.h
+++ lib.h
@@ -14,7 +14,7 @@ typedef struct Funcionario
     char codigo[7];
     char cpf[15];
     data nascimento;
-    char senha[25];
+    char senha[26];
     char contato[12];
 } funcionario[300];
 
@@ -25,32 +25,46 @@ typedef struct Produto
     float preco;
 } produto[300];
 
-char senha_adm[6]; // senha que ficara num arquivo separado para quando entrarem pela primeira vez nao precisarem de cadastro
+typedef struct Produto_entrada
+{
+    char nome[100];
+    int quantidade;
+    float preco_de_compra;
+} produto_entrada[300];
+
+typedef struct Produto_venda
+{
+    char nome[100];
+    int quantidade;
+    float preco_vendido;
+} produto_venda[300];
+
+char senha_adm[7]; // senha que ficara num arquivo separado para quando entrarem pela primeira vez nao precisarem de cadastro
 
 int produtos_existentes = 0; //tem a quantidade de produtos já cadastrados, essa quantidade é calculada no recuperar_entrada_produto() e aumentada/diminuida no cadastrar/excluir produto
 
 int funcionarios_existentes = 0; //tem a quantidade de funcionarios já cadastrados, essa quantidade é calculada no recuperar_funcionario() e aumentada/diminuida no cadastrar/excluir funcionario
 
-void recuperar_senha_adm(); // vai procurar a senha de adm no arquivo dela
-void recuperar_funcionario(); // vai procurar os funcionarios no arquivo que eles estao cadastrados
-void recuperar_produto(); // vai procurar os produtos no arquivo que eles estao cadastrados
-void recuperar_entrada_produto(); // vai procurar as entradas de produtos no arquivo que eles estao cadastrados *feito
-void recuperar_venda_produto(); // vai procurar as vendas de produtos no arquivo que eles estao cadastrados
-void menu_login(); // menu inicial onde voce pode entrar pelo seus dados de funcionario ou senha de adm
-void login_funcionario(); // login se quiser entrar como funcionario
-void login_adm(); // login se quiser entrar como adm
-void menu_funcionario(); // menu se entrar como funcionario (nao pode excluir/cadastrar funcionarios)
-void menu_adm(); // menu se entrar como adm (pode excluir/cadastrar funcionarios) 
-void menu_cadastro_e_exclusao_funcionario(); // menu que so aparece para adms
+void recuperar_senha_adm(); // vai procurar a senha de adm no arquivo dela *feito
+void recuperar_funcionario(); // vai procurar os funcionarios no arquivo que eles estao cadastrados *feito
+void recuperar_produto(); // vai procurar os produtos no arquivo que eles estao cadastrados *feito
+void recuperar_entrada_produto(); // vai procurar as entradas de produtos no arquivo que eles estao cadastrados *fzr Lara
+void recuperar_venda_produto(); // vai procurar as vendas de produtos no arquivo que eles estao cadastrados *fzr Emily
+void menu_login(); // menu inicial onde voce pode entrar pelo seus dados de funcionario ou senha de adm *fzr Iza
+void login_funcionario(); // login se quiser entrar como funcionario *feito
+void login_adm(); // login se quiser entrar como adm *fzr Gabriel
+void menu_funcionario(); // menu se entrar como funcionario (nao pode excluir/cadastrar funcionarios) *fzr Iza
+void menu_adm(); // menu se entrar como adm (pode excluir/cadastrar funcionarios) *fzr Iza
+void menu_cadastro_e_exclusao_funcionario(); // menu que so aparece para adms *fzr Iza
 void cadastro_funcionario(); // autoexplicativo *feito
-void salvar_funcionario(); // funcao para cadastrar os novos funcionarios no arquivo
-void exclusao_funcionario(); // autoexplicativo
-void menu_cadastro_e_exclusao_produto(); // autoexplicativo
-void cadastro_produto(); // autoexplicativo
+void salvar_funcionario(); // funcao para cadastrar os novos funcionarios no arquivo *feito
+void exclusao_funcionario(); // autoexplicativo *fzr Iza
+void menu_cadastro_e_exclusao_produto(); // autoexplicativo *fzr Iza
+void cadastro_produto(); // autoexplicativo *fzr Bia
 void salvar_produto(); // funcao para cadastrar os novos produtos no arquivo *feito
-void exclusao_produto(); // autoexplicativo
-void entrada_produto(); // autoexplicativo
-void salvar_entrada_produto(); // funcao para cadastrar a entrada de produtos no arquivo
-void venda_produto(); // autoexplicativo
-void salvar_venda_produto(); // funcao para cadastrar a venda de produtos no arquivo
-void extrato(); // autoexplicativo
+void exclusao_produto(); // autoexplicativo *fzr Bia
+void entrada_produto(); // autoexplicativo *fzr Lara
+void salvar_entrada_produto(); // funcao para cadastrar a entrada de produtos no arquivo *fzr Lara
+void venda_produto(); // autoexplicativo *fzr Emily
+void salvar_venda_produto(); // funcao para cadastrar a venda de produtos no arquivo *fzr Emily
+void extrato(); // autoexplicativo *fzr Gabriel
--- procedure.h
+++ procedure.h
@@ -1,17 +1,55 @@
 #include "lib.h"
-void recuperar_entrada_produto()
-{
-    FILE * arq = fopen("produtos.txt", "r");
 
+void recuperar_senha_adm()
+{
+    FILE * arq = fopen("senha_adm.txt", "r");
     if (arq == NULL)
     {
-        printf("Erro ao recuperar informaacoes do arquivo produtod.txt.\n");
+        printf("Erro ao recuperar informacoes do arquivo senha_adm.txt.\n");
         system("pause");
         system("cls");
         return;
     }
 
-    while (fscanf (arq, "Nome: %[^\n]\nCodigo: %[^\n]\nPreco de compra: %f\n\n", produto[i].nome, produto[i].codigo, &produto[i].preco) == 3)
+    fscanf(arq, "Ola admnistrador, digite sua senha a seguir (máximo de 6 digitos) : %[^\n]", senha_adm);
+
+    fclose(arq);
+
+}
+void recuperar_funcionario()
+{
+    FILE * arq = fopen("funcionarios.txt", "r");
+
+    if (arq == NULL)
+    {
+        printf("Erro ao recuperar informacoes do arquivo funcionarios.txt.\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nCPF: %[^\n]\nData de nascimento: %d/%d/%d\nSenha: %[^\n]\nTelefone: %[^\n]\n\n", funcionario[funcionarios_existentes].nome, funcionario[funcionarios_existentes].codigo, &funcionario[funcionarios_existentes].nascimento.dia, &funcionario[funcionarios_existentes].nascimento.mes, &funcionario[funcionarios_existentes].nascimento.ano, funcionario[funcionarios_existentes].senha, funcionario[funcionarios_existentes].contato) == 8)
+    {
+        i++;
+        funcionarios_existentes++;
+    };
+
+    fclose(arq);
+}
+
+void recuperar_produto()
+{
+    FILE * arq = fopen("produtos.txt", "r");
+
+    if (arq == NULL)
+    {
+        printf("Erro ao recuperar informacoes do arquivo produtos.txt.\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nPreco de compra: %f\n\n", produto[i].nome, produto[i].codigo, &produto[i].preco) == 3)
     {
         i++;
         produtos_existentes++;
@@ -20,13 +58,120 @@ void recuperar_entrada_produto()
     fclose(arq);
 }
 
-void salvar_produtos()
+void login_funcionario()
+{
+    char codigo_digitado[7];
+    char senha_digitada[25];
+
+    printf("Digite seu codigo de funcionario: ");
+
+    fflush(stdin);
+    gets(codigo_digitado);
+    system("cls");
+
+    printf("Digite sua senha: ");
+
+    fflush(stdin);
+    gets(senha_digitada);
+
+    system("cls");
+
+    for (int i = 0; i < funcionarios_existentes; i++)
+    {
+        if( strcmp (codigo_digitado, funcionario[i].codigo) == 0 && strcmp (senha_digitada, funcionario[i].senha) == 0) menu_funcionario();
+
+        else 
+        {
+            printf("Codigo de funcionario ou senha incorreta.\n");
+            system("pause");
+            system("cls");
+        }
+    }
+
+}
+
+void login_adm()
+{
+    char senha_digitada[7];
+    printf("Digite a senha de administrador: ");
+    
+    fflush(stdin);
+    gets(senha_digitada);
+
+    if (strcmp (senha_digitada, senha_adm) == 0) menu_adm();
+
+    else
+    {
+        printf("A senha digitada e a senha de adminstrador nao coincidem.\n");
+        system("pause");
+        system("cls");
+    }
+}
+
+void cadastro_funcionario()
+{
+    printf("Digite o nome do novo funcionario: ");
+    
+    fflush(stdin);
+    gets(funcionario[funcionarios_existentes].nome);
+
+    printf("Digite o codigo do novo funcionario (limite de 6 digitos) : ");
+    
+    fflush(stdin);
+    gets(funcionario[funcionarios_existentes].codigo);
+
+    printf("Digite o cpf do novo funcionario (inclua os sinais) : ");
+    
+    fflush(stdin);
+    gets(funcionario[funcionarios_existentes].cpf);
+
+    printf("Digite a data de nascimento do novo funcionario (inclua as barras) : ");
+
+    scanf("%d/%d/%d", &funcionario[funcionarios_existentes].nascimento.dia, &funcionario[funcionarios_existentes].nascimento.mes, &funcionario[funcionarios_existentes].nascimento.ano);
+
+    printf("Digite a senha do novo funcionario (maximo de 25 caracteres) : ");
+    
+    fflush(stdin);
+    gets(funcionario[funcionarios_existentes].senha);
+
+    printf("Digite o numero de telefone do novo funcionario (apenas os numeros) : ");
+    
+    fflush(stdin);
+    gets(funcionario[funcionarios_existentes].contato);
+
+    system("cls");
+
+    salvar_funcionario();
+
+    funcionarios_existentes++;
+
+}
+
+void salvar_funcionario()
+{
+    FILE * arq = fopen("funcionarios.txt", "a");
+
+    if (arq == NULL)
+    {
+        printf("Erro ao salvar novo produto no arquivo produtos.txt.\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    fprintf(arq, "Nome: %s\nCodigo: %s\nCPF: %s\nData de nascimento: %d/%d/%d\nSenha: %s\nTelefone: %s\n\n", funcionario[funcionarios_existentes].nome, funcionario[funcionarios_existentes].codigo, funcionario[funcionarios_existentes].nascimento.dia, funcionario[funcionarios_existentes].nascimento.mes, funcionario[funcionarios_existentes].nascimento.ano, funcionario[funcionarios_existentes].senha, funcionario[funcionarios_existentes].contato);
+
+    fclose(arq);
+
+}
+
+void salvar_produto()
 {
     FILE * arq = fopen("produtos.txt", "a");
 
     if (arq == NULL)
     {
-        printf("Erro ao abrir o arquivo.\n");
+        printf("Erro ao salvar novo produto no arquivo produtos.txt.\n");
         system("pause");
         system("cls");
         return;
@@ -35,45 +180,4 @@ void salvar_produtos()
     fprintf("Nome: %s\nCodigo: %s\nPreco de compra: %f\n\n", produto[produtos_existentes].nome, produto[produtos_existentes].codigo, produto[produtos_existentes].preco);
 
     fclose(arq);
-}
-
-void salvar_funcionario()
-{
-    
-}
-
-void cadastro_funcionario()
-{
-    printf("Digite o nome do novo funcionario: ");
-    
-    fflush(stdin);
-    gets(funcionario[produtos_existentes].nome);
-
-    printf("Digite o codigo do novo funcionario (limite de 6 digitos) : ");
-    
-    fflush(stdin);
-    gets(funcionario[produtos_existentes].codigo);
-
-    printf("Digite o cpf do novo funcionario (inclua os sinais) : ");
-    
-    fflush(stdin);
-    gets(funcionario[produtos_existentes].cpf);
-
-    printf("Digite a data de nascimento do novo funcionario (inclua as barras) : ");
-
-    scanf("%d/%d/%d", &funcionario[produtos_existentes].nascimento.dia, &funcionario[produtos_existentes].nascimento.mes, &funcionario[produtos_existentes].nascimento.ano);
-
-    printf("Digite a senha do novo funcionario (maximo de 25 caracteres) : ");
-    
-    fflush(stdin);
-    gets(funcionario[i].senha);
-
-    printf("Digite o numero de telefone do novo funcionario (apenas os numeros) : ");
-    
-    fflush(stdin);
-    gets(funcionario[i].contato);
-    salvar_funcionario();
-
-    produtos_existentes++;
-
 }
