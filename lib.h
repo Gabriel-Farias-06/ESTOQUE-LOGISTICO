54417c787f28715e46a96d5d7bb50cf4b93fd47f
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2024-08-13 08:59:40 -0300

    Update lib.h final

--- lib.h
+++ lib.h
@@ -45,6 +45,7 @@ struct Produto_venda
 char senha_adm[7]; // senha que ficara num arquivo separado para quando entrarem pela primeira vez nao precisarem de cadastro
 
 int produtos_existentes = 0; //tem a quantidade de produtos já cadastrados, essa quantidade é calculada no recuperar_entrada_produto() e aumentada/diminuida no cadastrar/excluir produto
+int produtos_comprados = 0;
 int produtos_vendidos = 0;
 int funcionarios_existentes = 0; //tem a quantidade de funcionarios já cadastrados, essa quantidade é calculada no recuperar_funcionario() e aumentada/diminuida no cadastrar/excluir funcionario
 
@@ -52,22 +53,23 @@ void recuperar_senha_adm(); // vai procurar a senha de adm no arquivo dela *feit
 void recuperar_funcionario(); // vai procurar os funcionarios no arquivo que eles estao cadastrados *feito
 void recuperar_produto(); // vai procurar os produtos no arquivo que eles estao cadastrados *feito
 void recuperar_entrada_produto(); // vai procurar as entradas de produtos no arquivo que eles estao cadastrados *feito
-void recuperar_venda_produto(); // vai procurar as vendas de produtos no arquivo que eles estao cadastrados *fzr Emily
+void recuperar_venda_produto(); // vai procurar as vendas de produtos no arquivo que eles estao cadastrados *feito
 void menu_login(); // menu inicial onde voce pode entrar pelo seus dados de funcionario ou senha de adm *feito
 void login_funcionario(); // login se quiser entrar como funcionario *feito
-int login_adm(); // login se quiser entrar como adm *fzr Gabriel
-void menu_funcionario(); // menu se entrar como funcionario (nao pode excluir/cadastrar funcionarios) *fzr Iza
-void menu_adm(); // menu se entrar como adm (pode excluir/cadastrar funcionarios) *fzr Iza
-void menu_cadastro_e_exclusao_funcionario(); // menu que so aparece para adms *fzr Iza
+void login_adm(); // login se quiser entrar como adm *feito
+void menu_funcionario(); // menu se entrar como funcionario (nao pode excluir/cadastrar funcionarios) *feito
+void menu_adm(); // menu se entrar como adm (pode excluir/cadastrar funcionarios) *feito
+void menu_cadastro_e_exclusao_funcionario(); // menu que so aparece para adms *feito
 void cadastro_funcionario(); // autoexplicativo *feito
 void salvar_funcionario(); // funcao para cadastrar os novos funcionarios no arquivo *feito
 void exclusao_funcionario(); // autoexplicativo *feito
-void menu_cadastro_e_exclusao_produto(); // autoexplicativo *fzr Iza
-void cadastro_produto(); // autoexplicativo *fzr Bia
+void menu_cadastro_e_exclusao_produto(); // autoexplicativo *feito
+void consulta_produto();
+void cadastro_produto(); // autoexplicativo *feito
 void salvar_produto(); // funcao para cadastrar os novos produtos no arquivo *feito
-void exclusao_produto(); // autoexplicativo *fzr Bia
+void exclusao_produto(); // autoexplicativo *feito
 void entrada_produto(); // autoexplicativo *feito
 void salvar_entrada_produto(); // funcao para cadastrar a entrada de produtos no arquivo *feito
-void venda_produto(); // autoexplicativo *fzr Emily
-void salvar_venda_produto(); // funcao para cadastrar a venda de produtos no arquivo *fzr Emily
-void extrato(); // autoexplicativo *fzr Gabriel
+void venda_produto(); // autoexplicativo *feito
+void salvar_venda_produto(); // funcao para cadastrar a venda de produtos no arquivo *feito
+void extrato(); // autoexplicativo *feito
