69b5ac8163d56741b1af3d6df8d6c477ee432880
Gabriel do Rego Farias <gabriel.rfarias@a.ucb.br>
2024-08-13 09:02:03 -0300

    Update procedure.h final

--- procedure.h
+++ procedure.h
@@ -1,47 +1,5 @@
 #include "lib.h"
 
-void salvar_venda_produto(){}//FAZER DEPOIS
-
-void venda_produto()//FAZER PRIMEIRO
-{
-    /*FILE *arq = fopen("venda_produto.txt", "a");
-    if (arq == NULL)
-    {
-        printf("Erro ao registrar informacoes de venda no arquivo venda_produto.txt.\n");
-        system("pause");
-        system("cls");
-        return;
-    }
-    printf ("Digite o nome do produto:\n");
-    fflush(stdin);
-    gets(Produto_venda.nome);
-    printf ("Digite a quantidade vendida:\n");
-    fflush(stdin);
-    gets(Produto_venda.quantidade);
-    */
-}
-
-void recuperar_venda_produto()//LER DO ARQUIVO E GUARDAR EM UMA VARIAVEL
-{
-    FILE *arq = fopen("venda_produto.txt", "r");
-    if (arq == NULL)
-    {
-        printf ("Erro ao abrir o arquivo.\n");
-        system("pause");
-        system("cls");
-        return;
-    }
-    while (3 == fscanf(arq, 
-    "Nome do produto: %100[^\n]\n"
-    "Quantidade de produtos vendidos: %d\n"
-    "Preco esperado de venda: %f\n\n"
-    produto[produtos_existentes].nome, &produto[produtos_existentes].quantidade, &produto[produtos_existentes].preco))
-    {
-        produtos_existentes++;
-    }
-    fclose(arq);
-}
-
 void recuperar_senha_adm()
 {
     FILE * arq = fopen("senha_adm.txt", "r");
@@ -58,6 +16,7 @@ void recuperar_senha_adm()
     fclose(arq);
 
 }
+
 void recuperar_funcionario()
 {
     FILE * arq = fopen("funcionarios.txt", "r");
@@ -70,12 +29,7 @@ void recuperar_funcionario()
         return;
     }
 
-    int i;
-    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nCPF: %[^\n]\nData de nascimento: %d/%d/%d\nSenha: %[^\n]\nTelefone: %[^\n]\n\n", funcionario[funcionarios_existentes].nome, funcionario[funcionarios_existentes].codigo, &funcionario[funcionarios_existentes].nascimento.dia, &funcionario[funcionarios_existentes].nascimento.mes, &funcionario[funcionarios_existentes].nascimento.ano, funcionario[funcionarios_existentes].senha, funcionario[funcionarios_existentes].contato) == 8)
-    {
-        i++;
-        funcionarios_existentes++;
-    };
+    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nCPF: %[^\n]\nData de nascimento: %d/%d/%d\nSenha: %[^\n]\nTelefone: %[^\n]\n\n", funcionario[funcionarios_existentes].nome, funcionario[funcionarios_existentes].codigo, &funcionario[funcionarios_existentes].nascimento.dia, &funcionario[funcionarios_existentes].nascimento.mes, &funcionario[funcionarios_existentes].nascimento.ano, funcionario[funcionarios_existentes].senha, funcionario[funcionarios_existentes].contato) == 8) funcionarios_existentes++;
 
     fclose(arq);
 }
@@ -92,13 +46,43 @@ void recuperar_produto()
         return;
     }
 
-    int i;
-    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nPreco de compra: %f\n\n", produto[i].nome, produto[i].codigo, &produto[i].preco) == 3)
-    {
-        i++;
-        produtos_existentes++;
-    };
+    while (fscanf(arq, "Nome: %[^\n]\nCodigo: %[^\n]\nPreco de compra: %f\n\n", produto[produtos_existentes].nome, produto[produtos_existentes].codigo, &produto[produtos_existentes].preco) == 3) produtos_existentes++;
 
+    fclose(arq);
+}
+
+void recuperar_entrada_produto()
+{
+    FILE * arq = fopen("entrada_produto.txt", "r");
+    if (arq == NULL)
+    {
+        printf("Erro ao recuperar irformacoes no arquivo produtos.txt. \n");
+        system("pause");
+        system("cls");
+        return;
+    }
+
+    while (fscanf(arq,"Nome: %[^\n]\nQuantidade: %[^\n]\nPreco de compra: %f\n\n", produto_entrada[produtos_comprados].nome, produto_entrada[produtos_comprados].quantidade, &produto_entrada[produtos_comprados].preco_de_compra) == 3) produtos_comprados++;
+
+    fclose(arq);
+}
+
+void recuperar_venda_produto()
+{
+    FILE *arq = fopen("venda_produto.txt", "r");
+    if (arq == NULL)
+    {
+        printf ("Erro ao abrir o arquivo.\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+    while (3 == fscanf(arq, 
+    "Nome do produto: %100[^\n]\n"
+    "Quantidade de produtos vendidos: %d\n"
+    "Preco esperado de venda: %f\n\n",
+    produto[produtos_existentes].nome, &produto_venda[produtos_existentes].quantidade, &produto[produtos_existentes].preco)) produtos_existentes++;
+    
     fclose(arq);
 }
 
@@ -134,7 +118,7 @@ void login_funcionario()
 
 }
 
-int login_adm()
+void login_adm()
 {
     char senha_digitada[7];
     printf("Digite a senha de administrador: ");
@@ -142,14 +126,14 @@ int login_adm()
     fflush(stdin);
     gets(senha_digitada);
 
-    if (strcmp (senha_digitada, senha_adm) == 0) return 1;
+    if (strcmp (senha_digitada, senha_adm) == 0) menu_adm;
 
     else
     {
         printf("A senha digitada e a senha de adminstrador nao coincidem.\n");
+
         system("pause");
         system("cls");
-        return 0;
     }
 
     
@@ -212,6 +196,65 @@ void salvar_funcionario()
 
 }
 
+void exclusao_funcionario()
+{
+    char codigoDig[7];
+    printf("Digite o codigo do funcionario que voce quer excluir: ");
+    
+    fflush(stdin);
+    gets(codigoDig);
+
+    for(int i = 0; i < funcionarios_existentes; i++)
+    {
+        if(strcmp(codigoDig, funcionario[i].codigo) == 0)
+        {
+            funcionarios_existentes--;
+
+            for(int j = i; j < funcionarios_existentes; j++) funcionario[j] = funcionario[j + 1];
+
+            FILE *arq = fopen ("produtos.txt", "w");
+            if (arq == NULL)
+            {
+                printf("Erro ao abrir o arquivo produtos.txt. \n");
+                system("pause");
+                system("cls");
+                return;
+            }
+
+            for (int i = 0; i < funcionarios_existentes; i++) fprintf(arq, "Nome: %s\nCodigo: %s\nCPF: %s\nData de nascimento: %d/%d/%d\nSenha: %s\nTelefone: %s\n\n", funcionario[funcionarios_existentes].nome, funcionario[funcionarios_existentes].codigo, funcionario[funcionarios_existentes].nascimento.dia, funcionario[funcionarios_existentes].nascimento.mes, funcionario[funcionarios_existentes].nascimento.ano, funcionario[funcionarios_existentes].senha, funcionario[funcionarios_existentes].contato);
+
+            fclose(arq);
+
+            printf("             Funcionario deletado com sucesso\n");
+            return;
+        }
+        
+    }
+    printf("             Funcionario nao registrada\n");
+    system("pause");
+    system("cls");
+}
+
+void cadastro_produto()
+{
+    printf("Digite o nome do novo produto: ");
+
+    fflush(stdin);
+    gets (produto[produtos_existentes].nome);
+
+    printf("Digite o codigo de identificacao do novo produto (6 digitos): ");
+
+    fflush(stdin);
+    gets (produto[produtos_existentes].codigo);
+
+    printf("Digite o preco de venda esperado do novo produto: ");
+
+    scanf("%f", &produto[produtos_existentes].preco);
+
+    salvar_produto();
+    produtos_existentes++;
+}
+
 void salvar_produto()
 {
     FILE * arq = fopen("produtos.txt", "a");
@@ -229,53 +272,70 @@ void salvar_produto()
     fclose(arq);
 }
 
-void recuperar_entrada_produto()
+void exclusao_produto()
 {
-    FILE * arq = fopen("produtos.txt", "r");
-    if (arq == NULL)
-    {
-        printf("Erro ao recuperar irformacoes no arquivo produtos.txt. \n");
-        system("pause");
-        system("cls");
-        return;
-    }
+    char codigo_digitado[7];
+    printf("Digite o codigo do produto a ser excluido: ");
 
-    int i;
-    while (fscanf(arq,"Nome: %[^\n]\nQuantidade: %[^\n]\nPreco de compra: %f\n\n", produto_entrada[i].nome, produto_entrada[i].quantidade, &produto_entrada[i].preco_de_compra) == 3);
+    fflush(stdin);
+    gets(codigo_digitado);
+
+    for (int i = 0; i < produtos_existentes; i++)
     {
-        i++;
-        produtos_existentes++;
+        if (strcmp(codigo_digitado, produto[i].codigo) == 0)
+        {
+            produtos_existentes--;
+
+            for (int j = i; j < produtos_existentes; j++) produto[j] = produto[j + 1];
+
+            FILE *arq = fopen ("produtos.txt", "w");
+            if (arq == NULL)
+            {
+                printf("Erro ao abrir o arquivo produtos.txt. \n");
+                system("pause");
+                system("cls");
+                return;
+            }
+
+            for (int i = 0; i < produtos_existentes; i++) fprintf(arq, "Nome: %s\nCodigo: %s\nPreco de compra: %f\n\n", produto[produtos_existentes].nome, produto[produtos_existentes].codigo, produto_entrada[produtos_existentes].preco_de_compra);
+
+            fclose(arq);
+
+            printf("            Produto deletado com sucesso\n");
+            return;
+        }
     }
-    fclose(arq);
+    printf ("           Codigo de produto nao registrado\n");
+    system("pause");
+    system("cls");
 }
+
 
 void entrada_produto()
 {
-    FILE * arq = fopen("produtos.txt", "a");
-
     printf("Digite o nome do produto:");
 
     fflush(stdin);
-    gets(produto_entrada[produtos_existentes].nome);
+    gets(produto_entrada[produtos_comprados].nome);
 
     printf("Digite a quantidade de produtos:");
 
-    scanf("%i", &produto_entrada[produtos_existentes].quantidade);
+    scanf("%i", &produto_entrada[produtos_comprados].quantidade);
 
     printf("Digite o preco de compra:");
 
-    scanf("%f", &produto_entrada[produtos_existentes].preco_de_compra);
+    scanf("%f", &produto_entrada[produtos_comprados].preco_de_compra);
 
     system("cls");
 
     salvar_entrada_produto();
     
-    produtos_existentes++;
+    produtos_comprados++;
 }
 
 void salvar_entrada_produto()
 {
-    FILE * arq = fopen("produtos.txt", "a");
+    FILE * arq = fopen("entrada_produto.txt", "a");
     if(arq == NULL)
     {
         printf("Erro ao salvar novo produto no produtos.txt.\n");
@@ -284,34 +344,162 @@ void salvar_entrada_produto()
         return;
     }
     
-    fprintf(arq, "Nome: %s\nQuantidade: %i\nPreco do produto:%f.", produto_entrada[produtos_existentes].nome, produto_entrada[produtos_existentes].quantidade, produto_entrada[produtos_existentes].preco_de_compra);
+    fprintf(arq, "Nome: %s\nQuantidade: %i\nPreco do produto:%f.", produto_entrada[produtos_comprados].nome, produto_entrada[produtos_comprados].quantidade, produto_entrada[produtos_comprados].preco_de_compra);
 
     fclose(arq);
 }
 
-void exclusao_funcionario()
+void venda_produto()
 {
-    boolean ver = 1;
-    char codigoDig[7];
-    if(login_adm()) 
+    printf ("Digite o nome do produto:\n");
+    
+    fflush(stdin);
+    gets(produto_venda[produtos_vendidos].nome);
+
+    printf ("Digite a quantidade vendida:\n");
+
+    fflush(stdin);
+    gets(produto_venda[produtos_vendidos].quantidade);
+
+    float total_produtos_vendidos = 0;
+    float total_produtos_comprados = 0;
+
+    for (int i = 0; i < produtos_vendidos; i++)
     {
-        printf("Digite o codigo do funcionario que voce quer excluir: ");
-        scanf("%d", &codigoDig); 
-        for(int i = 0; i <= funcionarios_existentes; i++)
+        if(strcmp(produto_venda[produtos_vendidos].nome, produto_entrada[i].nome) == 0)
         {
-            if(strcmp(codigoDig, funcionario[i].codigo) == 0)
-            {
-                for(int j = i; j < funcionarios_existentes; j++) funcionario[j] = funcionario[j + 1];
-                funcionarios_existentes--;
-                salvar_funcionario(funcionarios_existentes);
-                printf("             Funcionario deletado com sucesso\n");
-                ver = 0;
-                break;
-            }
-            
+            total_produtos_comprados += produto_entrada[i].quantidade;
         }
-    if(ver) printf("             Funcionario nao registrada\n");
+    }
+
+    for (int i = 0; i < produtos_comprados; i++)
+    {
+        if(strcmp(produto_venda[produtos_vendidos].nome, produto_venda[i].nome) == 0)
+        {
+            total_produtos_vendidos += produto_venda[i].quantidade;
+        }
+    }
+
+    if((total_produtos_comprados - total_produtos_vendidos) < produto_venda[produtos_existentes].quantidade)
+    {
+        printf("Quantidade maior do que a descrita no estoque, atualmente no estoque ha %d unidades.\n", total_produtos_comprados - total_produtos_vendidos);
+
+        system("pause");
+        system("cls");
+
+        return;
+    }
+
+    char opcao;
+
+    printf("O produto foi vendido pelo preco esperado? (s/n)\n\nDigite sua opcao: ");
+
+    scanf(" %c", &opcao);
+
+    if (opcao == 's') produto_venda[produtos_vendidos].preco_vendido = produto[produtos_existentes].preco;
+
+    else if (opcao == 'n')
+    {
+        printf("Digite o preco que o produto foi vendido: ");
+
+        scanf("%f", &produto_venda[produtos_vendidos].preco_vendido);
+    }
+    else
+    {
+        printf("Digite uma opcao valida!\n");
+        system("pause");
+        system("cls");
+        return;
+    }
+    salvar_venda_produto();
+    printf("            Registro da venda feito com sucesso\n");
     system("pause");
     system("cls");
+}
+
+void salvar_venda_produto()
+{
+    FILE * arq = fopen("venda_produto.txt", "a");
+    if (arq == NULL)
+    {
+        printf("Erro ao registrar informacoes de venda no arquivo venda_produto.txt.\n");
+        system("pause");
+        system("cls");
+        return;
     }
+
+    fprintf(arq, "Nome do produto: %s\nQuantidade de produtos vendidos: %d\nPreco esperado de venda: %f\n\n", produto[produtos_existentes].nome, produto_venda[produtos_existentes].quantidade, produto[produtos_existentes].preco);
+
+    fclose(arq);
+}
+
+void consulta_produto()
+{
+    boolean ver = 0;
+    float total_compra = 0;
+    float total_venda = 0;
+    float lucro;
+    int total_produtos_vendidos =  0;
+    int total_produtos_comprados = 0;
+    char produtoDigitado[100];
+    printf("Digite o nome do produto para consulta-lo: ");
+    
+    fflush(stdin);
+    gets(produtoDigitado);
+
+    for (int i = 0; i < produtos_comprados; i++)
+    {
+        if(strcmp(produtoDigitado, produto_entrada[i].nome) == 0)
+        {
+            total_produtos_comprados += produto_entrada[i].quantidade;
+            total_compra += produto_entrada[i].preco_de_compra * produto_entrada[i].quantidade;
+            ver = 1;
+        }
+    }
+    for (int i = 0; i < produtos_vendidos; i++)
+    {
+        if(strcmp(produtoDigitado, produto_venda[i].nome) == 0)
+        {
+            total_produtos_vendidos += produto_venda[i].quantidade;
+            total_venda += produto_venda[i].preco_vendido * produto_venda[i].quantidade;
+            ver = 1;
+        }
+    }
+    if(ver) printf("            Informacoes do produto %d\n\n\n1- Total de produtos comprados: %d.\n2- Total de produtos vendidos: %d.\n3- Total de valor gasto: %.2f.\n4- Total de valor ganho: %.2f.\n5- Total de lucro: %.2f.\n", produtoDigitado, total_produtos_comprados, total_produtos_vendidos, total_compra, total_venda, total_compra - total_venda);
+    else printf("Este produto ou nao foi cadastrado ou nao foi efetuada nenhuma venda/compra do mesmo.\n");
+    system("pause");
+    system("cls");
+}
+
+void extrato()
+{
+    int opcao;
+    printf("            Extrato\n\n1- Ver extrato geral\n2- Ver extrato de um produto especifico\n\nDigite sua opcao: ");
+    scanf("%d", &opcao);
+    if(opcao == 1)
+    {
+        for(int i = 0; i < produtos_vendidos; i++) printf("Nome do produto: %s. Quantidade vendida: %d. Preco vendido: %.2f.\n", produto_venda[i].nome, produto_venda[i].quantidade, produto_venda[i].preco_vendido);
+
+        for(int i = 0; i < produtos_comprados; i++) printf("Nome do produto: %s. Quantidade comprada: %d. Preco de compra: %.2f.\n", produto_entrada[i].nome, produto_entrada[i].quantidade, produto_entrada[i].preco_de_compra);
+    }
+    else if(opcao == 2)
+    {
+        char produtoDigitado[100];
+        printf("Digite o nome do produto para ver seu extrato: ");
+        
+        fflush(stdin);
+        gets(produtoDigitado);
+
+        for(int i = 0; i < produtos_comprados; i++)
+        {
+            if(strcmp(produtoDigitado, produto_entrada[i].nome) == 0) printf("Quantidade comprada: %d. Preco vendido: %.2f.\n", produto_entrada[i].quantidade, produto_entrada[i].preco_de_compra);
+        }
+
+        for(int i = 0; i < produtos_vendidos; i++)
+        {
+            if(strcmp(produtoDigitado, produto_venda[i].nome) == 0) printf("Quantidade vendida: %d. Preco vendido: %.2f.\n", produto_venda[i].quantidade, produto_venda[i].preco_vendido);
+        }
+    }
+    system("pause");
+    system("cls");
 }
